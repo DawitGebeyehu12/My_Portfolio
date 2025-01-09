60d854f04731ac394143821c0fad739e511e8ea5
DawitGebeyehu12 <125673841+DawitGebeyehu12@users.noreply.github.com>
2025-01-09 12:48:24 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,21 +7,21 @@ const About = () => {
       <h1 className="title">About Me</h1>
       <div className="section-container">
         <div className="section__pic-container">
-          <img src="/images/about-pic.jpg" alt="Profile picture" className="about-pic" />
+          <img src="/assets/images/about-pic.jpg" alt="Profile picture" className="about-pic" />
         </div>
         <div className="about-details-container">
           <div className="about-containers">
             <div className="details-container">
               <img src={EDU} alt="Education icon" className="icon" />
               <h3>Education</h3>
-              <p>B.Sc. Bachelors Degree in software engineering</p>
+              <p>B.Sc. Bachelor's Degree in Software Engineering</p>
             </div>
           </div>
           <div className="text-container">
             <p>
-            A frontend software engineer who combines creativity with technical expertise, 
-            I build engaging web experiences using HTML, CSS, and JavaScript. I specialize in
-             creating website with React.js and enhance performance through Next.js. 
+              I am a frontend software engineer who combines creativity with technical expertise. 
+              I build engaging web experiences using HTML, CSS, and JavaScript. I specialize in
+              creating websites with React.js and enhancing performance through Next.js.
             </p>
           </div>
         </div>
